ce214d904bbdc6a7b84baf46c172b73d21215a3d
dmkit <dm.bangayan@gmail.com>
2016-11-23 19:19:47 +0800

    initial commit

---
+++ src/Phalcon/Auth.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dmkit\Phalcon;
+
+class Auth
+{
+	public static function test()
+	{
+		echo 'yeah testing';
+	}
+}
---
+++ tests/AuthTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dmkit\Phalcon\Auth;
+
+class AuthTest extends PHPUnit_Framework_TestCase
+{
+	public function testTest()
+	{
+		$this->assertEquals('a', 'b');
+	}
+}
